4648bc65d39bbdfc109d0420e2377ed28830aab1
arturfil <arturfil@hotmail.com>
2024-07-12 04:23:36 -0400

    added completion, lsp-config, lualine, mano, & treesitter

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,24 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+        require("lualine").setup({
+            options = {
+                theme = "dracula",
+                section_separators = { left = '', right = '' },
+                sections = {
+                    lualine_a = {
+                        { 'mode', separator = { left = '' }, right_padding = 2 },
+                    },
+                    lualine_b = { 'filename', 'branch' },
+                    lualine_c = { 'fileformat' },
+                    lualine_x = {},
+                    lualine_y = { 'filetype', 'progress' },
+                    lualine_z = {
+                        { 'location', separator = { right = '' }, left_padding = 2 },
+                    },
+                }
+            }
+        })
+    end
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,29 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { 
+              "lua",
+				"javascript",
+				"typescript",
+				"python",
+				"go",
+				"sql",
+				"make",
+				"dockerfile",
+				"yaml",
+				"graphql",
+				"terraform",
+				"proto",
+				"html",
+          },
+          sync_install = false,
+          auto_install = true,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+}
